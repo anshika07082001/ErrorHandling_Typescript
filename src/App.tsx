50d6e82321b082a756a77b2d11a854a09b5e8dc3
anshikachaurasiya-cedcoss <anshikachaurasiya@cedcoss.com>
2023-01-09 12:39:59 +0530

    final

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,12 @@ import NumGeneratorFunc from './componentsFunc/NumGeneratorFunc';
 function App() {
   var [numClass,setNumClass]=useState(6)
   var [numFunc,setNumFunc]=useState(6)
+  // function for class component btn Click
   const clickHandler =()=>{
     var n = Math.floor(Math.random()*10)
     setNumClass(n)
   }
+  // function for functional component btn Click
   const clickHandlerFunc=()=>{
     var n = Math.floor(Math.random()*20)
     setNumFunc(n)
